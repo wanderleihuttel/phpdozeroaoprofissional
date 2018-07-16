3b5509e653ac12dc61e070f2a0bf66a262cf929f
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2018-07-16 19:16:40 -0300

    Portal Responsivo (3/3)

--- portal/assets/js/script.js
+++ portal/assets/js/script.js
@@ -1,26 +1,51 @@
-var slideitem = 0;
+var slideItem = 0;
 window.onload = function() {
    setInterval(passarSlide, 2000);
-   
+
    var slidewidth = document.getElementById("slideshow").offsetWidth;
    var objs = document.getElementsByClassName("slide");
    for(var i in objs) {
       objs[i].style.width = slidewidth;
+      console.log(slidewidth);
    }
 }
-function passarSlide(){
+function passarSlide() {
    var slidewidth = document.getElementById("slideshow").offsetWidth;
-   if(slideitem >= 3){
-      slideitem = 0;
+   
+   if(slideItem >= 3) {
+      slideItem = 0;
    } else {
-        slideitem++;
+      slideItem++;
    }
-   document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideitem + "px");
-}   
 
-
-function mudarSlide(pos){
-   slideitem = pos;
-   var slidewidth = document.getElementById("slideshow").offsetWidth;
-   document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-" + (slidewidth * slideitem + "px");
+   document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
 }
+function mudarSlide(pos) {
+   slideItem = pos;
+   var slidewidth = document.getElementById("slideshow").offsetWidth;
+   document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+}
+
+function toggleMenu() {
+
+   var menu = document.getElementById("menu");
+   console.log(menu);
+
+   if (menu.style.display == 'none' || menu.style.display == '') {
+      menu.style.display = "block";
+   } else {
+      menu.style.display = "none";
+   }
+
+}
+
+
+
+
+
+
+
+
+
+
+
