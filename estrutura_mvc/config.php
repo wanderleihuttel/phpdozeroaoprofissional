72a779c5b381d004bd35444a4138f87f319f79c3
Wanderlei Hüttel <wanderlei.huttel@gmail.com>
2018-09-26 20:35:29 -0300

    Módulo 16 - PHP Super Avançado - Aula 5 - Estrutura MVC: Index + Rewrite

---
+++ estrutura_mvc/config.php
@@ -0,0 +1,49 @@
+<?php
+require_once 'environment.php';
+
+
+$config = [];
+
+if ( ENVIRONMENT == 'development'){
+    define('BASE_URL', 'http://192.168.1.87/phpdozeroaoprofissional/estrutura_mvc/');
+    $config = [
+        'driver'   => 'mysql',
+        'host'     => 'localhost',
+        'port'     => 3306,
+        'dbname'   => 'estrutura_mvc',
+        'dbuser'   => 'cursophp',
+        'dbpass'   => 'cursophp',
+        'charset'  => 'utf8'
+    ];
+} else {
+    define('BASE_URL', 'http://localhost/estrutura_mvc/');
+    $config = [
+        'driver'   => 'mysql',
+        'host'     => 'localhost',
+        'port'     => 3306,
+        'dbname'   => 'estrutura_mvc',
+        'dbuser'   => 'cursophp',
+        'dbpass'   => 'cursophp',
+        'charset'  => 'utf8'
+    ];
+}
+
+global $db;
+try {
+
+    $dsn = "{$config['driver']}:dbname={$config['driver']};host={$config['host']};port={$config['port']};charset={$config['charset']};";
+    $dbuser = $config['dbuser'];
+    $dbpass = $config['dbpass'];
+    // PDO MySQL options
+    $options = array (
+        PDO::ATTR_CASE => PDO::CASE_LOWER,               // lowercase
+        PDO::ATTR_ERRMODE    => PDO::ERRMODE_EXCEPTION,  // pdo show exception
+        PDO::ATTR_PERSISTENT => true,                    // persistent connection
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // set fetch mode
+    );
+    // PDO object
+    $db = new PDO($dsn, $dbuser, $dbpass, $options);
+
+} catch (PDOException $e){
+    echo "(". $e->getCode() . ") " .$e->getMessage() . " at line: " . $e->getLine();
+}
---
+++ estrutura_mvc/index.php
@@ -0,0 +1,3 @@
+<?php
+session_start();
+require_once('config.php');
